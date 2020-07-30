1a714d8e36f3cd56b5c8346cb7e07c8da0bb0bc9
Clévisson <clevissonalgebra@gmail.com>
2020-07-30 20:59:51 -0300

    adicionando projeto

---
+++ src/main/kotlin/com/clevisson/blog/controlers/HtmlController.kt
@@ -0,0 +1,15 @@
+package com.clevisson.blog.controlers
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+    @GetMapping("/")
+    fun blog(model: Model): String{
+        model["title"] = "Blog"
+        return "blog"
+    }
+}
