4a81704f8ddd8582cd88e01592c7377cc3bf1e9c
Clévisson <clevissonalgebra@gmail.com>
2020-07-30 22:43:54 -0300

    Adicionando repositórios Spring Data JPA

---
+++ src/main/kotlin/com/clevisson/blog/Entities.kt
@@ -0,0 +1,23 @@
+package com.clevisson.blog
+
+import toSlug
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+class Article(
+        var title: String,
+        var headline: String,
+        var content: String,
+        @ManyToOne var author: User,
+        var slug: String = title.toSlug(),
+        var addedAt: LocalDateTime = LocalDateTime.now(),
+        @Id @GeneratedValue var id: Long? = null)
+
+@Entity
+class User(
+        var login: String,
+        var firstname: String,
+        var lastName: String,
+        var description: String? = null,
+        @Id @GeneratedValue var id: Long? = null)
